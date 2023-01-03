192cebd775cc3ea213ea03b60874d280994eff25
kojirou <kojirouhtc@gmail.com>
2023-01-04 01:28:11 +0800

    fix big error

--- Sources/JSON/Utils.swift
+++ Sources/JSON/Utils.swift
@@ -1,7 +1,7 @@
 extension StringProtocol {
   @inlinable
   func withCStringBuffer<R>(_ body: (UnsafeRawBufferPointer) throws -> R) rethrows -> R {
-    try withContiguousStorageIfAvailable { try body(UnsafeRawBufferPointer($0)) }
+    try utf8.withContiguousStorageIfAvailable { try body(UnsafeRawBufferPointer($0)) }
     ?? ContiguousArray(utf8).withUnsafeBytes(body)
   }
 }
