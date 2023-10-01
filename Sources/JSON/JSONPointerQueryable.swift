5447b1b61b38aabd0afd9f3a74582cb7aee51c79
kojirou <kojirouhtc@gmail.com>
2023-10-02 03:42:13 +0800

    more JSON Pointer APIs
    add set and remove APIs

--- Sources/JSON/JSONPointerQueryable.swift
+++ Sources/JSON/JSONPointerQueryable.swift
@@ -2,44 +2,77 @@ import yyjson
 
 public protocol JSONPointerQueryable {
   associatedtype QueuryResult: JSONValueProtocol
-  func query(jsonPointerBuffer: UnsafeRawBufferPointer) -> QueuryResult?
+  func value(byJSONPointer buffer: UnsafeRawBufferPointer) -> Result<QueuryResult, JSONPointerError>
+}
+
+public protocol JSONPointerSettable: JSONPointerQueryable {
+  // TODO: output context
+  func set(_ value: QueuryResult, byJSONPointer buffer: UnsafeRawBufferPointer, createParent: Bool) -> Result<Void, JSONPointerError>
+
+  // TODO: output context
+  func remove(byJSONPointer buffer: UnsafeRawBufferPointer) -> Result<QueuryResult, JSONPointerError>
 }
 
 extension JSON: JSONPointerQueryable {
   @inlinable
-  public func query(jsonPointerBuffer: UnsafeRawBufferPointer) -> JSONValue? {
-    yyjson_doc_ptr_getn(docPointer, jsonPointerBuffer.baseAddress, jsonPointerBuffer.count)
-      .map { JSONValue($0, self) }
+  public func value(byJSONPointer buffer: UnsafeRawBufferPointer) -> Result<JSONValue, JSONPointerError> {
+    var err = yyjson_ptr_err()
+    return yyjson_doc_ptr_getx(docPointer, buffer.baseAddress, buffer.count, &err)
+      .map { .success(.init($0, self)) } ?? .failure(.init(err))
   }
 }
 
-extension MutableJSON: JSONPointerQueryable {
+extension MutableJSON: JSONPointerSettable {
   @inlinable
-  public func query(jsonPointerBuffer: UnsafeRawBufferPointer) -> MutableJSONValue? {
-    yyjson_mut_doc_ptr_getn(docPointer, jsonPointerBuffer.baseAddress, jsonPointerBuffer.count)
-      .map { .init($0, self) }
+  public func value(byJSONPointer buffer: UnsafeRawBufferPointer) -> Result<MutableJSONValue, JSONPointerError> {
+    var err = yyjson_ptr_err()
+    // TODO: support context
+    return yyjson_mut_doc_ptr_getx(docPointer, buffer.baseAddress, buffer.count, nil, &err)
+      .map { .success(.init($0, self)) } ?? .failure(.init(err))
+  }
+
+  @inlinable
+  public func set(_ value: MutableJSONValue, byJSONPointer buffer: UnsafeRawBufferPointer, createParent: Bool) -> Result<Void, JSONPointerError> {
+    var err = yyjson_ptr_err()
+    return yyjson_mut_doc_ptr_setx(docPointer, buffer.baseAddress, buffer.count, value.valPointer, createParent, nil, &err) ? .success(()) : .failure(.init(err))
+  }
+
+  @inlinable
+  public func remove(byJSONPointer buffer: UnsafeRawBufferPointer) -> Result<MutableJSONValue, JSONPointerError> {
+    var err = yyjson_ptr_err()
+    return yyjson_mut_doc_ptr_removex(docPointer, buffer.baseAddress, buffer.count, nil, &err)
+      .map { .success(.init($0, self)) } ?? .failure(.init(err))
   }
 }
 
 extension JSONValue: JSONPointerQueryable {
   @inlinable
-  public func query(jsonPointerBuffer: UnsafeRawBufferPointer) -> JSONValue? {
-    yyjson_ptr_getn(valPointer, jsonPointerBuffer.baseAddress, jsonPointerBuffer.count)
-      .map { .init($0, document) }
+  public func value(byJSONPointer buffer: UnsafeRawBufferPointer) -> Result<JSONValue, JSONPointerError> {
+    var err = yyjson_ptr_err()
+    return yyjson_ptr_getx(valPointer, buffer.baseAddress, buffer.count, &err)
+      .map { .success(.init($0, document)) } ?? .failure(.init(err))
   }
 }
 
-extension MutableJSONValue: JSONPointerQueryable {
+extension MutableJSONValue: JSONPointerSettable {
   @inlinable
-  public func query(jsonPointerBuffer: UnsafeRawBufferPointer) -> MutableJSONValue? {
-    yyjson_mut_ptr_getn(valPointer, jsonPointerBuffer.baseAddress, jsonPointerBuffer.count)
-      .map { .init($0, document) }
+  public func value(byJSONPointer buffer: UnsafeRawBufferPointer) -> Result<MutableJSONValue, JSONPointerError> {
+    var err = yyjson_ptr_err()
+    // TODO: support context
+    return yyjson_mut_ptr_getx(valPointer, buffer.baseAddress, buffer.count, nil, &err)
+      .map { .success(.init($0, document)) } ?? .failure(.init(err))
   }
-}
 
-extension JSONPointerQueryable {
   @inlinable
-  public func query(jsonPointer: some StringProtocol) -> QueuryResult? {
-    jsonPointer.withCStringBuffer(query(jsonPointerBuffer:))
+  public func set(_ value: MutableJSONValue, byJSONPointer buffer: UnsafeRawBufferPointer, createParent: Bool) -> Result<Void, JSONPointerError> {
+    var err = yyjson_ptr_err()
+    return yyjson_mut_ptr_setx(valPointer, buffer.baseAddress, buffer.count, value.valPointer, document.docPointer, createParent, nil, &err) ? .success(()) : .failure(.init(err))
+  }
+
+  @inlinable
+  public func remove(byJSONPointer buffer: UnsafeRawBufferPointer) -> Result<MutableJSONValue, JSONPointerError> {
+    var err = yyjson_ptr_err()
+    return yyjson_mut_ptr_removex(valPointer, buffer.baseAddress, buffer.count, nil, &err)
+      .map { .success(.init($0, document)) } ?? .failure(.init(err))
   }
 }
